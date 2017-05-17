310428f3e7f69bd73574d3a71b4e1c5986037878
zhan <chenyunzhan08@126.com>
2017-05-17 17:19:38 +0800

    登录界面

---
+++ PigHouse/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  PigHouse
+//
+//  Created by zhan on 2017/5/17.
+//  Copyright © 2017年 zhan. All rights reserved.
+//
+
+import Foundation
+
+
+class LoginViewController: UIViewController {
+    
+    @IBAction func cancelAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
